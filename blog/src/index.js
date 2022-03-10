88827ba7405b579a439959109753b5c2e50a5efa
Seung-hwan285 <ghks285@naver.com>
2022-03-10 21:42:39 +0900

    feat : 이전 값 저장하기

--- blog/src/index.js
+++ blog/src/index.js
@@ -13,9 +13,6 @@ function Square(props) {
         );
 }
 
-
-
-
 function calculateWinner(squares) {
     const lines = [
         [0, 1, 2],
@@ -35,59 +32,22 @@ function calculateWinner(squares) {
     }
     return null;
 }
+
 // 부모
 // 사각형 9개 랜더링
 class Board extends React.Component {
 
-
-    constructor(props) {
-        super(props);
-
-        this.state={
-            squares: Array(9).fill(null),
-            xIsNext : true,
-        };
-    }
-
-    handlerClick(i){
-        const squares = this.state.squares.slice();
-
-
-        squares[i]= this.state.xIsNext ? 'X':'O';
-
-        console.log(squares[i])
-        this.setState({
-            squares:squares,
-
-            // 클릭했을때 값 변하게 만들어줌 true => false , false => true
-            xIsNext : !this.state.xIsNext,
-        });
-    }
-
     renderSquare(i) {
-        return <Square value={this.state.squares[i]}
-            onClick={()=>this.handlerClick(i)}
+        return <Square value={this.props.squares[i]}
+            onClick={()=>this.props.onClick(i)}
         />
     }
 
 
     render() {
 
-        let status;
-        //우승자 출력
-        const winner = calculateWinner(this.state.squares);
- 
-
-        if(winner){
-            status = 'Winner: '+winner;
-        }else {
-            status = 'Next Player : ' + (this.state.xIsNext ? 'X' : 'O')
-        }
-
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -111,14 +71,72 @@ class Board extends React.Component {
 // game 게임판을 렌더링하며 자리 표시자 값을 가지고 있다.
 
 class Game extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        // 초기 state 설정
+        this.state={
+            history :[{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext : true,
+        }
+    }
+    handlerClick(i){
+
+
+        const history = this.state.history;
+        const current = history[history.length-1];
+
+
+        const squares = current.squares;
+
+
+        // 현재 우승자가 나오면 종료
+        if(calculateWinner(squares)){
+            return;
+        }
+
+
+        //현재 인덱스에 xIsNext 값을 판별
+        squares[i]=this.state.xIsNext ? 'X':'O';
+
+
+        // state 업데이트
+        this.setState({
+           history : history.concat([{
+               squares : squares,
+           }]),
+            // true 였으면 false, false 였으면 true
+           xIsNext : !this.state.xIsNext,
+        });
+    }
+
     render() {
+
+        const history = this.state.history;
+        const current = history[history.length -1];
+
+        const winner = calculateWinner(current.squares);
+
+        let status;
+
+        if(winner){
+            status='Winner'+winner;
+        }else{
+            status='Next Player '+(this.state.xIsNext ? 'X':'O');
+        }
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                    squares ={current.squares}
+                    onClick={(i) =>this.handlerClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
+                    <div>{status}</div>
                     <ol>{/* TODO */}</ol>
                 </div>
             </div>
