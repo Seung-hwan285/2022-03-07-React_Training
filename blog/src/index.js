87bf89c0a8150324448f268f5a47625a93ea1492
Seung-hwan285 <ghks285@naver.com>
2022-03-19 16:11:10 +0900

    refactor : 함수형 변환 클릭하면 X,O 변환

--- blog/src/index.js
+++ blog/src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import React, { useState } from 'react'
 
-
+// Board 상태가 변화될때 Square 컴포넌트는 다시 랜더링된다.
 // 자식 <button> 랜더링
 const Square=(props)=>{
 
-    const [message , setMessage] = useState({value:null});
 
     return (
         <button className="square"
@@ -17,16 +16,35 @@ const Square=(props)=>{
     )
 }
 
+
+
 // 부모 사각형 9개 랜더링
-const Board = ()=>{
-    
-    const [message , setMessage] = useState({squares : Array(9).fill(null)});
+const Board = (props)=>{
+
+
+    const [message , setMessage] = useState({
+        squares : Array(9).fill(null),
+
+        // x, o 클릭할때마다 바뀌가 boolean 변수 추가
+        xNext : true,
+    });
 
     const handleClick=(i)=>{
-        const value = message.squares.slice();
 
-        value[i]='X';
-        setMessage({squares: value})
+        // 불변성때문에 .slice 연산자를 사용해서 배열의 복사본 생성
+        // 불변성? 데이터를 직접 변경하지 않고 사본을 만들고나서 데이터를 교체하는 방식
+        // 왜 불변성 사용? -> 변하지 않는 데이터는 변경이 이루어졌는지 판단이 쉽고 이를 바탕으로 컴포넌트가 다시 렌더링할지 결정
+        // 즉 리액트에서 순수 컴포넌트를 만드는데 도움을 준다.
+        const sliceSquares = message.squares.slice();
+
+        console.log(message.xNext)
+
+        sliceSquares[i]=  message.xNext ? 'X':'O';
+
+        setMessage({
+            squares: sliceSquares,
+            xNext: !message.xNext,
+        });
     }
 
     // 1번 값들을 받아와서 2
@@ -39,9 +57,20 @@ const Board = ()=>{
 
     }
 
-    const status = `Next Player : `;
+
+    const winner  =calculateWinner(message.squares);
+
+
+    let status;
+    if(winner){
+        status= 'Winner' + winner;
+    }else{
+        status='Next Player' +message.xNext ? 'X':'O';
+    }
+
     // 여기서 값이 전달되고 1
     return (
+
         <div>
             <div className="status">{status}</div>
             <div className="board-row">
@@ -79,26 +108,26 @@ const Game=()=>{
         </div>
     );
 }
-//
-// function calculateWinner(squares) {
-//     const lines = [
-//         [0, 1, 2],
-//         [3, 4, 5],
-//         [6, 7, 8],
-//         [0, 3, 6],
-//         [1, 4, 7],
-//         [2, 5, 8],
-//         [0, 4, 8],
-//         [2, 4, 6],
-//     ];
-//     for (let i = 0; i < lines.length; i++) {
-//         const [a, b, c] = lines[i];
-//         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-//             return squares[a];
-//         }
-//     }
-//     return null;
-// }
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
 
 
 // ========================================
