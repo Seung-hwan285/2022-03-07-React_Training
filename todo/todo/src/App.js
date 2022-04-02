4eb04bf1d8ebca2fa4c34250264f7826aaf7f41c
Seung-hwan285 <ghks285@naver.com>
2022-04-02 21:02:39 +0900

    docs : TodoList docs문서 작성

--- todo/todo/src/App.js
+++ todo/todo/src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import TodoItems from "./component/TodoItem/TodoItems";
 import {search} from "./component/Weather/Fetch.js";
+import WeatherRender from "./component/Weather/WeatherRender";
 
 
 
 function App() {
-    search();
+
   return (
     <div className="App">
       <TodoItems/>
+        <WeatherRender/>
     </div>
   );
 }
---
+++ todo/todo/src/component/Weather/WeatherRender.js
@@ -0,0 +1,16 @@
+import {Search, search} from "./Fetch.js";
+
+const WeatherRender =()=>{
+
+
+
+    return(
+        <div>
+            <span>{Search()}</span>
+
+        </div>
+    )
+
+}
+
+export default WeatherRender;
