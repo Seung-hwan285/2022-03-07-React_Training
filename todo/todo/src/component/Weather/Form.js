8c659ae0439df23986cca606486d6df615c24f73
Seung-hwan285 <ghks285@naver.com>
2022-04-06 18:28:07 +0900

    todo-feat : 구름 아이콘 설정

--- todo/todo/src/component/Weather/Form.js
+++ todo/todo/src/component/Weather/Form.js
@@ -1,13 +1,14 @@
 import React, {useEffect, useState} from "react";
 
 const api ={
-    API_KEY : 'cd439d0d8386ee860ee3c082e826e1ca',
+    API_KEY : 'a536cbda68541f2066e2a1fd221d55d5',
     BASE: 'https://api.openweathermap.org/data/2.5/',
 };
 
 const Form =()=>{
 
     const [data,setData]=useState([]);
+
 
     let componentMouted=true;
 
@@ -18,20 +19,33 @@ const Form =()=>{
            if(componentMouted){
                setData(await response.json());
            }
-           return ()=>{
-               componentMouted=false;
-           }
+
+           // state가 렌더링(마운트)하기전에 동작하기때문에 아무것도 안나오는 현상발생 (값을 읽게 만들어줘야함)
+           componentMouted=false;
        }
+
        fetchWeather();
+
    },[]);
 
+
+   const [Icon, setIcon]=useState("");
+
+   useEffect(()=>{
+       if(data.weather){
+           console.log(data.weather[0]);
+           setIcon(data.weather[0].icon);
+       }
+   },[data]);
+
+   console.log(Icon)
+
+
    // let temp = (data.main.temp-273.15).toFixed(2);
-
     console.log(data)
-
    return(
-        <div className="weather-container">
 
+        <div className="weather-container">
             <div className="top">
                 <div className="location">
                     {data.name}
@@ -42,12 +56,13 @@ const Form =()=>{
                 </div>
 
                 <div className="description">
-                    Clouds
+                    <img src={"http://openweathermap.org/img/w/" + Icon + ".png"}/>
                 </div>
             </div>
 
         </div>
-    )
+    );
+
 }
 
 export default Form;
