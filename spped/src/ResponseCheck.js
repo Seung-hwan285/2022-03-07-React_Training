c53b4047bd0271d992ce781203a0c279a7cec85f
Seung-hwan285 <ghks285@naver.com>
2022-06-06 20:13:57 +0900

    feat : 클릭했을때 색상 집어넣기

--- spped/src/ResponseCheck.js
+++ spped/src/ResponseCheck.js
@@ -14,17 +14,27 @@ const ResponseCheck =()=>{
 
     const onClickScreen =()=>{
 
-
-
         // 파란화면일때 클릭하는경우
         // 준비하세요 -> 빨간화면이 나와야함
         if(value === 'waiting'){
             setValue('ready');
             setMessage('초록색이 되면 클릭');
-        }else{
-            setValue('waiting');
-            setMessage('클릭')
+
+            setTimeout(()=>{
+                setValue('now');
+                setMessage('지금 클릭');
+            },Math.floor(Math.random()*1000)+2000);
         }
+        // 성급하게 클릭
+        else if(value==='ready'){
+            setValue('waiting');
+            setMessage('너무 성급하세요!');
+        }
+        else if(value==='now'){ // 반응속도 체크
+            setValue('waiting');
+            setMessage('클릭해서 시작하세요');
+        }
+
     }
 
 
